d7b110ac7fb5af65c29c07de36669e0ba5da83b9
Ysafi1 <kabandasafi18@gmail.com>
2023-11-28 17:44:20 +0200

    src files updated

--- src/app.py
+++ src/app.py
@@ -32,3 +32,32 @@ def predict_sespis_infec(sepsiss_features:smartfeatures):
     predict = pipeline.predict(df)[0]
 
     return {"Prediction": predict}
+
+
+
+
+#############
+@app.post('/predict_infection')
+def predict_sepsis_infection(sepsis_features: SmartFeatures):
+    try:
+        df = pd.DataFrame([sepsis_features.dict()])
+        
+        # Flatten the data
+        flattened_data = df.values.flatten()
+
+        # Create a new DataFrame with the flattened data and use numeric column names
+        df_for_prediction = pd.DataFrame([flattened_data], columns=[f"col_{i}" for i in range(len(flattened_data))])
+
+        # Log dimensions before prediction
+        logging.debug(f"Shape before prediction: {df_for_prediction.shape}")
+
+        # Prediction
+        predict = pipeline.predict(df_for_prediction)
+        # Log dimensions after prediction
+        logging.debug(f"Shape after prediction: {predict.shape}")
+
+        return {"Prediction": predict[0]}
+    
+    except Exception as e:
+        logging.error(f"Prediction error: {str(e)}", exc_info=True)
+        raise HTTPException(status_code=500, detail="Internal Server Error")

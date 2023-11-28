fbbb947a2c35acb31f7b6abb717c4d27617f036d
Ysafi1 <kabandasafi18@gmail.com>
2023-11-28 11:15:48 +0200

    init Fastapi

--- Api.py
+++ Api.py
@@ -9,10 +9,14 @@ logging.basicConfig(level=logging.DEBUG)
 pipeline = joblib.load('./dev/pipeline2.joblib')
 encoder = joblib.load('./dev/encoder.joblib')
 
-app = FastAPI()
+app = FastAPI(
+    title= "Sepsis Analysis",
+    description= "This App Is For Sepsis Analysis"
+)
 
+# Input Features
 class SmartFeatures(BaseModel):
-    PRG: int
+    PRG: int 
     PL: int
     PR: int
     SK: int
@@ -21,7 +25,7 @@ class SmartFeatures(BaseModel):
     BD2: float
     Age: int
     Insurance: int
-    Sepssis: int
+    #Sepssis: object
     
 @app.get('/')
 def read_root():
@@ -33,10 +37,25 @@ def predict_sepsis_infection(sepsis_features: SmartFeatures):
         df = pd.DataFrame([sepsis_features.dict()])
         
         # encoded_data = encoder.transform(df)
-        predict = pipeline.predict(df)[0]
-        return {"Prediction": predict}
+        
+        flattened_data = df.values.flatten()
+        
+        df_for_prediction = pd.DataFrame([flattened_data], columns=df.columns)
+        
+        # Log dimensions before prediction
+        logging.debug(f"Shape before prediction: {df_for_prediction.shape}")
+
+        #Prediction
+        predict = pipeline.predict(df_for_prediction.values.reshape(1, -1))
+        
+        # Log dimensions after prediction
+        logging.debug(f"Shape after prediction: {predict.shape}")
+        
+        return {"Prediction": predict[0]}
+    
     except Exception as e:
         logging.error(f"Prediction error: {str(e)}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
+# Additional routes and functionality can be added as needed
 
